c08ea6bb569e55df6b12532b1e7f4eaa1151528d
Allan Khebab <allan.khebab@edu.igensia.com>
2024-10-04 14:41:29 +0200

    premier tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,3 +4,12 @@ from olympics import db
 def test_countries():
     rows = db.get_countries()
     assert len(rows) > 100
+
+def test_countries_avec_id():
+    rows = db.get_countries(id=1)
+    assert len(rows) == 1
+       
+def test_athletes():
+    rows = db.get_athletes()
+    assert len(rows) 
+    
