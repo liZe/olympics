51b32b8dbc97d1a108f7d856199195cf877df43a
Allan Khebab <allan.khebab@edu.igensia.com>
2024-10-07 19:56:39 +0200

    tests pour api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,25 @@
 from fastapi.testclient import TestClient
-
 from olympics import api
 
-
 client = TestClient(api.app)
-
 
 def test_countries():
     response = client.get('/countries/')
     assert response.status_code == 200
     assert len(response.json()) > 100
+
+def test_athletes():
+    response = client.get('/athletes/')
+    assert response.status_code == 200
+    assert len(response.json()) > 100  
+
+def test_disciplines():
+    response = client.get('/disciplines/')
+    assert response.status_code == 200
+    assert len(response.json()) > 10  
+
+def test_teams():
+    response = client.get('/teams/')
+    assert response.status_code == 200
+    assert len(response.json()) > 5  
+
