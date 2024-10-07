d75645630688048edce5727981f742efce2475ab
Allan Khebab <allan.khebab@edu.igensia.com>
2024-10-07 20:13:42 +0200

    tests pour api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -23,3 +23,45 @@ def test_teams():
     assert response.status_code == 200
     assert len(response.json()) > 5  
 
+def test_events():
+    response = client.get('/events/')
+    assert response.status_code == 200
+    assert len(response.json()) > 20  
+
+def test_medals():
+    response = client.get('/medals/')
+    assert response.status_code == 200
+    assert len(response.json()) > 10  
+
+def test_discipline_athletes():
+    discipline_id = 1
+    response = client.get(f'/discipline-athletes/{discipline_id}')
+    assert response.status_code == 200
+    assert len(response.json()) > 0  
+    
+def test_top_countries():
+    response = client.get('/top-countries/')
+    assert response.status_code == 200
+    assert len(response.json()) > 5 
+
+def test_collective_medals():
+    team_id = 1
+    response = client.get(f'/collective-medals/?team_id={team_id}')
+    assert response.status_code == 200
+    assert len(response.json()) > 0  
+    
+def test_top_collective():
+    response = client.get('/top-collective/')
+    assert response.status_code == 200
+    assert len(response.json()) > 5  
+
+def test_individual_medals():
+    athlete_id = 1
+    response = client.get(f'/individual-medals/?athlete_id={athlete_id}')
+    assert response.status_code == 200
+    assert len(response.json()) > 0  
+    
+def test_top_individual():
+    response = client.get('/top-individual/')
+    assert response.status_code == 200
+    assert len(response.json()) > 5  
