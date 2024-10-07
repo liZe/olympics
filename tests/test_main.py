e84fc14fa97bfa6bf531f24719b799a0dbe515e8
Allan Khebab <allan.khebab@edu.igensia.com>
2024-10-07 20:19:56 +0200

    ajout de test pour main.py avec tests qui fails

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,4 +3,30 @@ from olympics.__main__ import main
 
 def test_countries():
     argv = ['countries']
-    main(argv)
+    main(argv)  
+
+
+def test_collective():
+    argv = ['collective', '--top', '5']
+    main(argv)  
+
+
+def test_individual():
+    argv = ['individual', '--top', '3']
+    main(argv)  
+
+
+def test_negative_top():
+    argv = ['countries', '--top', '-5']
+    try:
+        main(argv)
+    except SystemExit as e:
+        assert str(e) == "1"  
+
+
+def test_zero_top():
+    argv = ['individual', '--top', '0']
+    try:
+        main(argv)
+    except SystemExit as e:
+        assert str(e) == "1"  # Cela va check si le code de sortie est 1 pour une erreur
